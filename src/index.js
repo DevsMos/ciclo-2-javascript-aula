392b5b5ff18979937f9eab5f9e109784d9966927
user.name <emanuel.sinik@hotmail.com>
2024-03-14 19:11:57 -0300

    adicionando variavel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-var valorA;
-valorFinal = valorA;
-varMoi = "7"
-console.log(varMoi);
-console.log("hello world");
+// var valorA;
+// valorFinal = valorA;
+// varMoi = "7"
+// console.log(varMoi);
+// console.log("hello world");
+
+
+
+var valorA = 7; 
+var valorB = 9;
+var valorFinal = valorA + valorB;
+console.log(valorFinal)
